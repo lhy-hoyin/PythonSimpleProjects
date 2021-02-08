b99ec5d2adb31c97461e222e5d9aac87e95a49df
BarryHY <67904634+BarryHY@users.noreply.github.com>
2021-02-08 21:59:04 +0800

    Added 2-player mode, included a main menu. Some code optmised.

--- Hangman.py
+++ Hangman.py
@@ -3,15 +3,18 @@ Author: BarryHY
 Date Created: 29 Jan 2021
 Date Updated: 08 Feb 2021
 Project Name: Hangman
-Description: Guess the word within the given attempts/lives
+Description: Guess the word within the given attempts/lives. Multiple modes available.
 
 Library Required: RandomWords (https://pypi.org/project/RandomWords/)
 '''
 
+from getpass import getpass
 from random_words import RandomWords
 
-# CONSTANTS - may be customised accordingly
+# Game Parameters - may be customised accordingly
 LIVES = 6
+BONUS_LIVES = 4
+ENABLE_HINTS = True
 LIVES_UNICODE = u'\u2665' #heart icon
 
 def main():
@@ -19,35 +22,60 @@ def main():
     print("= = = HANGMAN = = =")
     print("===================")
 
-    # TODO : select playing mode (i.e. AI or 2-player)
+    # Game Loop
+    while (True):
+        # Display main menu to select playing mode
+        print("\n[Main Menu]")
+        print("[1] Play against computer")
+        print("[2] Play against another player")
+        print("[Q] Quit Game")
+        
+        choice = input(">>>")[0] # only take first char
+        
+        # Play against computer
+        if choice == '1':
+            # Randomly generate a word
+            randomWord = RandomWords().random_word()
+            
+            # Start game with the random word (with bonus lives)
+            guess_the_word(randomWord, lives = LIVES + BONUS_LIVES)
+            
+        # Play against another player
+        if choice == '2':
+            # Prompt player 1 to provide input
+            print("\nPlayer One,")
+            givenWord = getpass("Please enter a WORD for Player Two to guess: (just type, word is hidden)")
+            #FIXME : world should be only alphabets (i.e not spaces,digits,dots,dashes, etc) , maybe check spelling too
+            givenHint = input("Please enter a HINT to help Player Two: ")
+            
+            # Start game with player 1's word
+            print("\nPlayer Two,")
+            guess_the_word(givenWord, hint = givenHint)
+        
+        # Quit game
+        if choice.upper() == 'Q':
+            print("Thank you for playing!")
+            break
 
-    # Randomly generate a word
-    mysteryWord = RandomWords().random_word()
-    
-    # Start the game
-    guess_the_word(mysteryWord)
-
-def guess_the_word(guessWord:str, lives:int = LIVES):
-    
-    livesCount = lives
+def guess_the_word(mysteryWord:str, hint:str = None, lives:int = LIVES):
     solvedFlag = False
     
-    print("You have %d attempts to guess the word." % livesCount)
-    print("Enter any single digit to quit.") #FIXME
+    print("\nYou have %d lives to guess the word." % lives)
+    print("Enter any single digit to go back to main menu.")
 
-    # Convert guessWord to list of dict
+    # Convert mysteryWord to list of dict
     word = []
-    for char in guessWord.upper():
+    for char in mysteryWord.upper():
         word.append({char : False}) # False flag to hide letter
 
     # Continue game if player still have lives
-    while (livesCount > 0 and not solvedFlag):
+    while (lives > 0 and not solvedFlag):
         correctGuess = False # reset flag
         print() # creates newline
 
         # Display lives left
         print("Life: ", end = '')
-        for x in range(livesCount):
+        for x in range(lives):
             print(LIVES_UNICODE, end=" ") 
         print() # creates newline
         
@@ -57,13 +85,16 @@ def guess_the_word(guessWord:str, lives:int = LIVES):
             print(list(char.keys())[0] if list(char.values())[0] else '*', end = '')
         print() # creates newline
 
+        if(ENABLE_HINTS and hint != None):
+            print("Hint: " + hint)
+
         # Takes in user input
         userInput = input("Choose a SINGLE letter or type out your WHOLE guess: ")
         
         # Player choose to guess the whole word
         if len(userInput) > 1:
             # Player guessed correctly
-            if guessWord.upper() == userInput.upper():
+            if mysteryWord.upper() == userInput.upper():
                 correctGuess = True
                 solvedFlag = True
 
@@ -74,7 +105,7 @@ def guess_the_word(guessWord:str, lives:int = LIVES):
         
             # Check for exiting condition
             if letter.isnumeric():
-                print("You have entered a digit. Quitting...")
+                print("You have entered a digit. Back to main menu...")
                 break; # exit game loop
             
             solvedFlag = True # set flag for condition checking later
@@ -98,21 +129,16 @@ def guess_the_word(guessWord:str, lives:int = LIVES):
         
         # Deduct live for incorrect guess
         if not correctGuess:
-            livesCount = livesCount - 1
+            lives -= 1
             print("Sorry, wrong guess. You lost a life.") 
-
-        print() # create newline
 
     # Player has solved the mystery word
     if solvedFlag:
-        print("Congratulations! You solved \'%s\' with %d %s left." % (guessWord, livesCount, ("lives" if livesCount > 1 else "life")))
+        print("Congratulations! You solved \'%s\' with %d %s left." % (mysteryWord, lives, ("lives" if lives > 1 else "life")))
 
-    # Player has lost all lives
-    if livesCount < 1:
-        print("Game Over! You have no lives left.")
-
-    # Finish game
-    print("Thank you for playing!")
+    # Player has lost all lives, reveal mysteryWord
+    if lives < 1:
+        print("Game Over! You have no lives left. The mystery word is \'%s\'." % mysteryWord)
 
 if __name__ == "__main__":
     main()
